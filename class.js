3e2700c00865b38842eb1717ff4b61ee2e455857
JOÃO BATISTA XAVIER VELOSO JÚNIOR <joao_veloso-junior1@estudante.sesisenai.org.br>
2024-03-06 09:25:12 -0300

    terminado atualiza class

--- class.js
+++ class.js
@@ -18,26 +18,42 @@ des_carro(){
 }
 
 atual_carro(){
-
+    this.x += this.dir
+        if(this.x <=2){
+            this.x = 2
+        }else if(this.x >= 416){
+            this.x = 416
+        }
 }
 
 point(Objeto){
-
+    if((objeto.y > 610) && (objeto.y < 618)){
+        return true
+    }
 }
 
 colid(Objeto){
-
+    if((this.x < objeto.x + objeto.w)&&
+        (this.x + this.w > objeto.x)&&
+        (this.y < objeto.y + objeto.h)&&
+        (this.y + this.h > objeto.y)){
+            return true
+        }
 }
 
 }
 
 class Inimigo extends Carro{
     atual_inimigo(){
-
+        this.y += 1
+        if(this.y >= 780){
+            this.recomeca()
+        }
     }
 
     recomeca(){
-
+        this.y = -100
+        this.x = Math.floor(Math.random() * ((416 - 2 + 1) + 2)) // quando o carro sair da tela
     }
  }
 
